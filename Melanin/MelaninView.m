93230c18ad8fbc87d08b37b815fd91972ec7b296
Tobias Pape <tobias@netshed.de>
2012-08-24 08:13:12 +0200

    Initial Commit

---
+++ Melanin/MelaninView.m
@@ -0,0 +1,52 @@
+//
+//  MelaninView.m
+//  Melanin
+//
+//  Created by Tobias Pape on 24.08.12.
+//  Copyright (c) 2012 Tobias Pape. All rights reserved.
+//
+
+#import "MelaninView.h"
+
+@implementation MelaninView
+
+- (id)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
